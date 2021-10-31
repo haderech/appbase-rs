b4354fb35d0e46baff4ef8b8c4f3bab465fe6985
Jeeyong Um <conr2d@gmail.com>
2021-10-31 15:46:03 +0900

    Use downcast-rs instead of mopa

--- appbase/src/plugin.rs
+++ appbase/src/plugin.rs
@@ -1,4 +1,4 @@
-use mopa::mopafy;
+use downcast_rs::{Downcast, impl_downcast};
 
 pub trait Base {
    fn type_name() -> &'static str where Self: Sized;
@@ -8,13 +8,13 @@ pub trait Base {
    fn _shutdown(&mut self);
 }
 
-pub trait Plugin: mopa::Any + Send+ Sync + Base {
+pub trait Plugin: Downcast + Send + Sync + Base {
    fn new() -> Self where Self: Sized;
    fn init(&mut self) {}
    fn startup(&mut self) {}
    fn shutdown(&mut self) {}
 }
-mopafy!(Plugin);
+impl_downcast!(Plugin);
 
 #[derive(PartialEq, Copy, Clone, Debug)]
 pub enum State {
